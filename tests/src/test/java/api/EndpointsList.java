ca27c6155e464ed0af6320b26a0f5399cff0c6fb
Ekaterina.Makarova <ma_ka_r0va@bk.ru>
2025-01-12 11:57:35 +0100

    Remove static final modifiers as not needed

--- tests/src/test/java/api/EndpointsList.java
+++ tests/src/test/java/api/EndpointsList.java
@@ -1,7 +1,7 @@
 package api;
 
 public interface EndpointsList {
-    static final String SUPPORTED_CURRENCIES_ENDPOINT = "http://{appHost}:{appPort1}/supported-currency";
-    static final String SUPPORTED_CURRENCY_ENDPOINT = "http://{appHost}:{appPort1}/supported-currency/{currency-code}";
-    static final String CURRENCY_RATES_ENDPOINT = "http://{appHost}:{appPort1}/rates/{currency-code}/{YYYY-MM-DD}";
+    String SUPPORTED_CURRENCIES_ENDPOINT = "http://{appHost}:{appPort1}/supported-currency";
+    String SUPPORTED_CURRENCY_ENDPOINT = "http://{appHost}:{appPort1}/supported-currency/{currency-code}";
+    String CURRENCY_RATES_ENDPOINT = "http://{appHost}:{appPort1}/rates/{currency-code}/{YYYY-MM-DD}";
 }
