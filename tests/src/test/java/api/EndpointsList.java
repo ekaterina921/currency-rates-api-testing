a4475d36d2877dfc316adc580ab29b9d8ccd53b0
Ekaterina.Makarova <ma_ka_r0va@bk.ru>
2025-02-11 16:38:11 -0600

    Update EndpointsList

--- tests/src/test/java/api/EndpointsList.java
+++ tests/src/test/java/api/EndpointsList.java
@@ -4,7 +4,8 @@ import static api.BaseTestStartEnd.appHost;
 import static api.BaseTestStartEnd.appPort;
 
 public interface EndpointsList {
-    String SUPPORTED_CURRENCIES_ENDPOINT = "http://" + appHost + ":" + appPort + "/supported-currency";
-    String SUPPORTED_CURRENCY_ENDPOINT = "http://" + appHost + ":" + appPort + "/supported-currency/{currency-code}";
-    String CURRENCY_RATES_ENDPOINT = "http://" + appHost + ":" + appPort + "/rates/{currency-code}/{YYYY-MM-DD}";
+    String BASEURI = "http://" + appHost + ":" + appPort;
+    String SUPPORTED_CURRENCIES_ENDPOINT = "/supported-currency";
+    String SUPPORTED_CURRENCY_ENDPOINT = "/supported-currency/{currency-code}";
+    String CURRENCY_RATES_ENDPOINT = "/rates/{currency-code}/{YYYY-MM-DD}";
 }
