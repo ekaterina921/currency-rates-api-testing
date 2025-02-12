267342f8f0e909569e4201df74ec2446590f42de
Ekaterina.Makarova <ma_ka_r0va@bk.ru>
2025-02-11 20:04:19 -0600

    Refactor list of constants

--- tests/src/test/java/api/EndpointsList.java
+++ tests/src/test/java/api/EndpointsList.java
@@ -5,7 +5,7 @@ import static api.BaseTestStartEnd.appPort;
 
 public interface EndpointsList {
     String BASEURI = "http://" + appHost + ":" + appPort;
-    String SUPPORTED_CURRENCIES_ENDPOINT = "/supported-currency";
-    String SUPPORTED_CURRENCY_ENDPOINT = "/supported-currency/{currency-code}";
-    String CURRENCY_RATES_ENDPOINT = "/rates/{currency-code}/{YYYY-MM-DD}";
+    String SUPPORTED_CURRENCIES_ENDPOINT = BASEURI + "/supported-currency";
+    String SUPPORTED_CURRENCY_ENDPOINT = BASEURI + "/supported-currency/{currency-code}";
+    String CURRENCY_RATES_ENDPOINT = BASEURI + "/rates/{currency-code}/{YYYY-MM-DD}";
 }
