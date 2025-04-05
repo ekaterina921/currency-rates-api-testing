67b84e4819a551ff4d75d32da1c680de7282f01a
Ekaterina.Makarova <ma_ka_r0va@bk.ru>
2025-04-05 12:03:46 -0500

    Fix MongoDB checks for Currency Rates db Add tests to verify response in case of incorrect currency codes and dates

--- tests/src/test/java/api/ApiTests.java
+++ tests/src/test/java/api/ApiTests.java
@@ -3,6 +3,7 @@ package api;
 import Utilities.Utils;
 
 import com.mongodb.reactivestreams.client.MongoCollection;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.reactivestreams.Publisher;
 import org.bson.Document;
 
@@ -75,6 +76,11 @@ public class ApiTests extends BaseTestStartEnd implements EndpointsList {
     @ParameterizedTest
     @CsvFileSource(resources = "SupportedCurrenciesWithPastDates.csv")
     public void testGetCurrencyEndpointPastDates(String currencyCode, String pastDate) {
+        mainDB = mongoClientMainDB.getDatabase("CurrencyRates");
+        MongoCollection<Document> collectionLogs = mainDB.getCollection(currencyCode.toLowerCase());
+        Publisher<Long> ratesDoc = collectionLogs.countDocuments();
+        var beforeRatesNum = Mono.from(ratesDoc).block();
+        beforeRatesNum = (beforeRatesNum== null) ? 0L : beforeRatesNum;
         //Send request
         var response = given()
                 .when()
@@ -85,13 +91,42 @@ public class ApiTests extends BaseTestStartEnd implements EndpointsList {
                 .assertThat()
                 .statusCode(200)
                 .body(matchesJsonSchemaInClasspath("api/Json_Schema_Currency_Rates.json"));
-        mainDB = mongoClientMainDB.getDatabase("CurrencyRates");
-        MongoCollection<Document> collectionLogs = mainDB.getCollection(currencyCode.toLowerCase());
- //       String id = String.format("%s 00:00:00.000+00:00", pastDate);
-        Publisher<Long> ratesDoc = collectionLogs.countDocuments();
+        //Validate Database records
+        collectionLogs = mainDB.getCollection(currencyCode.toLowerCase());
+        ratesDoc = collectionLogs.countDocuments();
         var ratesNum = Mono.from(ratesDoc).block();
         Assertions.assertNotNull(ratesNum, "The number of currency rates is null. It should be > 0.");
-        Assertions.assertTrue(ratesNum.intValue() > 0, "There should be more than 0 records in Currency Rates.");
+        Assertions.assertTrue(ratesNum > beforeRatesNum, "There should be more records in Currency Rates database.");
+    }
+
+    @DisplayName("Verify that duplicates are not saved in the Currency Rates database.")
+    @ParameterizedTest
+    @CsvSource({ "EUR, 2025-02-01"})
+    public void testGetCurrencyEndpointRequestingTheSameDatesSeveralTimes(String currencyCode, String pastDate) {
+        mainDB = mongoClientMainDB.getDatabase("CurrencyRates");
+        //request <Past Date> for the first time
+        given()
+                .when()
+                .get(CURRENCY_RATES_ENDPOINT, currencyCode, pastDate);
+        MongoCollection<Document> collectionLogs = mainDB.getCollection(currencyCode.toLowerCase());
+        Publisher<Long> ratesDoc = collectionLogs.countDocuments();
+        var beforeRatesNum = Mono.from(ratesDoc).block();
+        Assertions.assertNotNull(beforeRatesNum, "The number of currency rates is null. It should be > 0.");
+        //Send request for <Past Date> for the second time
+        var response = given()
+                .when()
+                .get(CURRENCY_RATES_ENDPOINT, currencyCode, pastDate);
+        //Validate response
+        response
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("api/Json_Schema_Currency_Rates.json"));
+        //Validate Database records
+        ratesDoc = collectionLogs.countDocuments();
+        var ratesNum = Mono.from(ratesDoc).block();
+        Assertions.assertNotNull(ratesNum, "The number of currency rates is null. It should be > 0.");
+        Assertions.assertEquals(ratesNum, beforeRatesNum, "The number of records shouldn't have changed in Currency Rates database.");
     }
 
     @DisplayName("Verify valid date boundary value is correctly processed by Supported Currencies endpoint.")
@@ -125,7 +160,6 @@ public class ApiTests extends BaseTestStartEnd implements EndpointsList {
                 .body("error", equalTo("Date is not in the past."));
     }
 
-
     @DisplayName("Verify that Supported Currencies With Future Dates endpoint returns error.")
     @ParameterizedTest
     @CsvFileSource(resources = "SupportedCurrenciesWithFutureDates.csv")
@@ -140,6 +174,37 @@ public class ApiTests extends BaseTestStartEnd implements EndpointsList {
                 .assertThat()
                 .statusCode(400)
                 .body("error", equalTo("Date is not in the past."));
+    }
+
+    @DisplayName("Verify that proper error message is displayed for unsupported currencies.")
+    @ParameterizedTest
+    @CsvSource({ "RSD, 2025-02-01"})
+    public void testGetCurrencyEndpointUnsupportedCurrencies(String currencyCode, String pastDate) {
+        //Send request
+        var response = given()
+                .when()
+                .get(CURRENCY_RATES_ENDPOINT, currencyCode, pastDate);
+        //Validate response
+        response
+                .then()
+                .assertThat()
+                .statusCode(400)
+                .body("error", equalTo(String.format("%s is not supported", currencyCode)));
+    }
+
+    @DisplayName("Verify that application does not fail if non-parsable date format is used.")
+    @ParameterizedTest
+    @CsvSource({ "EUR, Mar"})
+    public void testGetCurrencyEndpointWrongDateFormat(String currencyCode, String wrongDate) {
+        //Send request
+        var response = given()
+                .when()
+                .get(CURRENCY_RATES_ENDPOINT, currencyCode, wrongDate);
+        //Validate response
+        response
+                .then()
+                .assertThat()
+                .statusCode(400);
     }
 
     @DisplayName("Verify that application logs are recorded in the database.")
