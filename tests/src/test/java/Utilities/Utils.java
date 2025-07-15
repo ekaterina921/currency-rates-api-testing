b6e49ba82103972188608e0b7f99f91a6b67a890
Ekaterina.Makarova <ma_ka_r0va@bk.ru>
2025-07-15 14:47:47 -0500

    Update Utils.java

--- tests/src/test/java/Utilities/Utils.java
+++ tests/src/test/java/Utilities/Utils.java
@@ -6,18 +6,18 @@ import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Utils {
-    public String getYesterdaysDate(){
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern( "uuuu-MM-dd" );
+    public String getYesterdaysDate() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("uuuu-MM-dd");
         return ZonedDateTime.now().withZoneSameInstant(ZoneOffset.UTC).minusDays(1).format(formatter);
     }
 
-    public String getTodaysDate(){
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern( "uuuu-MM-dd" );
+    public String getTodaysDate() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("uuuu-MM-dd");
         return ZonedDateTime.now().withZoneSameInstant(ZoneOffset.UTC).format(formatter);
     }
 
     public Instant reformatDateForMongoSearch(String pastDate) {
-        String dayId =  pastDate + "T00:00:00.000+00:00";
+        String dayId = pastDate + "T00:00:00.000+00:00";
         return Instant.parse(dayId);
     }
 }
